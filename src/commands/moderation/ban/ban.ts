e7b3b6b9fc3b035383a6bb172258ee915a10b37d
iDiego5614 <diego5614yt@gmail.com>
2022-06-28 22:58:48 -0500

    feat(Root): 👮 Moderation | Ban Command Part 1°

---
+++ src/commands/moderation/ban/ban.ts
@@ -0,0 +1,8 @@
+import { ICallback } from "types/ICommand"
+
+const ban : ICallback = (interaction) =>
+{
+  interaction.reply("BANNN")
+}
+
+export default ban
---
+++ src/commands/moderation/ban/index.ts
@@ -0,0 +1,30 @@
+import { ICommand } from "types/ICommand";
+import ban from "./ban";
+
+export default {
+  options : {
+    name : "ban",
+    description : "Con este comando podrás banear a un usuario en especifico de tu servidor.",
+    options : [
+      {
+        name : "user",
+        description : "Usuario a banear.",
+        type : "USER",
+        required : true
+      },
+      {
+        name : "reason",
+        description : "Razón por la cual será baneado este usuario.",
+        type : "STRING",
+        required : false
+      },
+      {
+        name : "time",
+        description : "Tiempo por el cual será baneado éste usuario.",
+        type : "STRING",
+        required : false
+      }
+    ]
+  },
+  command : ban
+} as ICommand
