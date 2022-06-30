8122eaacd9bc7bb77250286f36663a451e84cced
iDiego5614 <diego5614yt@gmail.com>
2022-06-29 23:15:08 -0500

    feat(Root): 💵 Economy | Default Max and Min amounts

---
+++ src/utils/amounts.ts
@@ -0,0 +1,18 @@
+export interface IAmounts
+{
+  work : number,
+  crime : number,
+  fight : number
+}
+
+export const MIN_AMOUNTS : IAmounts = {
+  work : 500,
+  crime : 700,
+  fight : 1000
+}
+
+export const MAX_AMOUNTS : IAmounts = {
+  work : 2500,
+  crime : 3500,
+  fight : 6000
+}
