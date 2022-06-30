b7d059fac954dc1e516ff5879994c1280ccc0418
iDiego5614 <diego5614yt@gmail.com>
2022-06-29 23:10:11 -0500

    chore(Root): 🧠 User Schema

---
+++ src/schemas/UserSchema.ts
@@ -0,0 +1,30 @@
+import { Schema , Document , model } from "mongoose"
+
+export interface IUser extends Document
+{
+  userId : string,
+  guildId : string,
+  cash : number,
+  bank : number
+}
+
+const UserSchema = new Schema<IUser>({
+  userId : {
+    type : String ,
+    required : true
+  },
+  guildId : {
+    type : String ,
+    required : true
+  },
+  cash : {
+    type : Number ,
+    default : 1000
+  },
+  bank : {
+    type : Number ,
+    default : 0
+  }
+} , { timestamps : true })
+
+export default model("users" , UserSchema)
